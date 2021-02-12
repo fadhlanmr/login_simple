d7f5122ec356101ffb33611761cc25a9d9f8062b
Muhammad Fadhlan Min Robby <fadhlan.mr@gmail.com>
2021-02-12 08:32:23 +0700

    quick add

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import Axios from 'axios'
 import App from './App.vue'
+import router from './router'
+
+Vue.prototype.$http = Axios;
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,82 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
+import Register from '@/components/Register.vue'
+import Dashboard from '@/components/Dashboard.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+      if (localStorage.getItem('jwt') == null) {
+          next({
+              path: '/login',
+              params: { nextUrl: to.fullPath }
+          })
+      } else {
+          let user = JSON.parse(localStorage.getItem('user'))
+          if(to.matched.some(record => record.meta.is_admin)) {
+              if(user.is_admin == 1){
+                  next()
+              }
+              else{
+                  next({ name: 'userboard'})
+              }
+          }else {
+              next()
+          }
+      }
+  } else if(to.matched.some(record => record.meta.guest)) {
+      if(localStorage.getItem('jwt') == null){
+          next()
+      }
+      else{
+          next({ name: 'userboard'})
+      }
+  }else {
+      next()
+  }
+})
+
+export default router
